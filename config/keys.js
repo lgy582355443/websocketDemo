c6ccf815736e81ee9eeeadd1636cdfa918bd1cbd
LiGaoYang <582355443@qq.com>
2020-03-29 02:18:11 +0800

    websocket练习

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // mongoURI: 'mongodb://test:123abc@ds227853.mlab.com:27853/wchart-api',
+  mongoURI: 'mongodb://localhost/websockDemo',
+  // mongoURI: 'mongodb://wxapi:test1234@ds135983.mlab.com:35983/wx-api',
+  secretOrKey: 'secret'
+};
